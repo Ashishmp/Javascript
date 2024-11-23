8c86248f40125d40f54da2816827228ab8764d9b
Ashishmp <ar9034@srmist.edu.in>
2024-11-23 13:52:10 +0530

    update

---
+++ basics2/scope.js
@@ -0,0 +1,12 @@
+
+
+if(true){
+    let a = 20 
+    const b =10
+   // var c = 30  // var or without any datatype act as globally declaration 
+}
+{}// this called as scope
+
+console.log(a);
+console.log(b);
+console.log(c);
