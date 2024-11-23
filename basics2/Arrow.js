ee7e7052db09529f629326128687d32e5f0a2152
Ashishmp <ar9034@srmist.edu.in>
2024-11-23 14:18:05 +0530

    upd

--- basics2/Arrow.js
+++ basics2/Arrow.js
@@ -12,3 +12,31 @@ const user = {
 // user.welcomeMessege()
 // user.username = "Sam"
 // user.welcomeMessege()
+
+
+//console.log(this);
+
+// function one(){
+//     let username = "Ashish"
+//    // console.log(this.username);
+// }
+
+
+const one = () => {
+    let username = "Ashish"
+    console.log(this.username);
+}
+//one()
+
+//Arrow function
+
+// const add2 = (num1, num2) =>{
+//     return num1+num2
+// }
+//object decared under paranthesis
+const add2 = (num1, num2) => num1+num2
+console.log(add2(4,6));
+
+
+// const myArray = [2,3,4,5,6]
+// myArray.forEach()
